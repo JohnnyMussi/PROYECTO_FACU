3a5786869d0169dc09fb0e6a60cbb76ec70fd319
JohnnyEBM <jony.mussi@hotmail.com>
2025-08-12 23:51:31 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto_Facu/Controllers/AccesoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Facu.Models;
+
+namespace Proyecto_Facu.Controllers
+{
+    public class AccesoController : Controller
+    {
+
+        static string cadena = "\"Data Source=MSI\\SQLEXPRESS;Initial Catalog=DB_ACCESO;Integrated Security=True";
+
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+        
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+
+
+    }
+}
